6e6e3a64d3c39dddf1baebf025847100f33c967e
Marcos Câmara <marcosramosvc@gmail.com>
2018-06-16 12:04:25 -0300

    Criação da classe Util para métodos auxiliares.

--- src/main/java/br/com/validacaoxml/ValidadorApplication.java
+++ src/main/java/br/com/validacaoxml/ValidadorApplication.java
@@ -2,12 +2,10 @@ package br.com.validacaoxml;
 
 import java.io.File;
 
-import javax.swing.JOptionPane;
-
-import org.apache.commons.lang3.StringUtils;
 import org.xml.sax.SAXParseException;
 
 import br.com.validacaoxml.exeception.InfraExecption;
+import br.com.validacaoxml.util.Util;
 import br.com.validacaoxml.validacao.ValidacaoXML;
 
 public class ValidadorApplication {
@@ -23,42 +21,16 @@ public class ValidadorApplication {
 			
 			File arquivoXml = new File("shiporder_sem_erro.xml");
 			File arquivoXsd = new File("shiporder.xsd");
+			
 			validacao.valida(arquivoXml, arquivoXsd);
-			JOptionPane.showMessageDialog(null, "XML validado com sucesso!","Sucesso", JOptionPane.INFORMATION_MESSAGE);
+			
+			Util.exibirDialogoSucesso("XML validado com sucesso!");
 		} catch (InfraExecption ie) {
-			JOptionPane.showMessageDialog(null, ie.getMessage(),"Erro", JOptionPane.ERROR_MESSAGE);
+			Util.exibirDialogoErro(ie.getMessage());
 		} catch (SAXParseException se) {
-			JOptionPane.showMessageDialog(null, obterMsgFormatada(se),"Erro", JOptionPane.ERROR_MESSAGE);
+			Util.exibirDialogoErro(Util.obterMsgFormatada(se));
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null, ex.getMessage(),"Erro", JOptionPane.ERROR_MESSAGE);
-			System.out.println("Erro: " + ex.getMessage());
+			Util.exibirDialogoErro(ex.getMessage());
 		}
-	}
-
-	private static void validarArgumentos(String[] args) throws InfraExecption {
-		
-		if(args.length == 0) {
-			throw new InfraExecption("Argumentos sao obrigatorios. Primeiro argumento caminho xml e segundo argumento caminho do xsd");	
-			
-		}else if(StringUtils.isNotBlank(args[0]) && StringUtils.isBlank(args[1])) {
-			throw new InfraExecption("O caminho do xsd deve ser informado!");
-			
-		}else if(StringUtils.isBlank(args[0]) && StringUtils.isNotBlank(args[1])) {
-			throw new InfraExecption("O caminho do xml deve ser informado!");
-		}
-	}
-	
-	private static String obterMsgFormatada(SAXParseException se) {
-		StringBuilder msgFormatada = new StringBuilder();
-		msgFormatada.append("Linha do erro: ");
-		msgFormatada.append(se.getLineNumber());
-		msgFormatada.append("\n");
-		msgFormatada.append("Coluna do erro: ");
-		msgFormatada.append(se.getColumnNumber());
-		msgFormatada.append("\n");
-		msgFormatada.append("Erro: ");
-		msgFormatada.append(se.getMessage());
-		return msgFormatada.toString();
-	}
-	
+	}	
 }
---
+++ src/main/java/br/com/validacaoxml/util/Util.java
@@ -0,0 +1,55 @@
+package br.com.validacaoxml.util;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xml.sax.SAXParseException;
+
+import br.com.validacaoxml.exeception.InfraExecption;
+/**
+ * Classe Util.
+ * @author Marcos
+ *
+ */
+public class Util {
+	
+	/**
+	 * Formata a mensagem em caso de erro de validação do XML.
+	 * @param se objeto do tipo SAXParseException
+	 * @return mensagem formatada
+	 */
+	public static String obterMsgFormatada(SAXParseException se) {
+		StringBuilder msgFormatada = new StringBuilder();
+		msgFormatada.append("Linha do erro: ");
+		msgFormatada.append(se.getLineNumber());
+		msgFormatada.append("\n");
+		msgFormatada.append("Coluna do erro: ");
+		msgFormatada.append(se.getColumnNumber());
+		msgFormatada.append("\n");
+		msgFormatada.append("Erro: ");
+		msgFormatada.append(se.getMessage());
+		return msgFormatada.toString();
+	}
+	
+	public static void exibirDialogoErro(String mensagem) {
+		JOptionPane.showMessageDialog(null, mensagem,"Erro", JOptionPane.ERROR_MESSAGE);
+	}
+	
+	public static void exibirDialogoSucesso(String mensagem) {
+		JOptionPane.showMessageDialog(null, mensagem,"Sucesso", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	public static void validarArgumentos(String[] args) throws InfraExecption {
+		
+		if(args == null || args.length == 0) {
+			throw new InfraExecption("Argumentos sao obrigatorios. Primeiro argumento caminho xml e segundo argumento caminho do xsd");	
+			
+		}else if(StringUtils.isNotBlank(args[0]) && StringUtils.isBlank(args[1])) {
+			throw new InfraExecption("O caminho do xsd deve ser informado!");
+			
+		}else if(StringUtils.isBlank(args[0]) && StringUtils.isNotBlank(args[1])) {
+			throw new InfraExecption("O caminho do xml deve ser informado!");
+		}
+	}
+
+}
